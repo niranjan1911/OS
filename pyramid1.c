e9541b59a6c1a8271987f50390969300dca9bf5a
niranjan1911 <52923629+niranjan1911@users.noreply.github.com>
2019-08-10 20:41:15 +0530

    Add files via upload

---
+++ pyramid1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+     int i,j,n,a=1,k,temp;
+     scanf("%d",&n);
+     temp=n;
+     for(i=1;i<=n;i++)	
+     {  
+	      for(k=i;k<temp;k++)
+	      {
+			printf(" ");
+	       
+	       }
+	       for(j=0;j<a;j++)
+	       {
+			 printf("%d",j+1);
+		 
+	       }
+		 
+	       a=a+2;
+	       printf("\n");
+     }   
+}
